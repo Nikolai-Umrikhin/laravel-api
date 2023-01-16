d380ba478dbde7a23ccaf7051b56194d9ea3f123
nick <itsjustme@Ya.ru>
2023-01-16 18:00:19 +0300

    added PUT and PATCH method to the customers API

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -81,7 +81,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
